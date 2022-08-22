5ac5c7a3b50682ea9fe2d9d6e0a392092c6deb5e
Charles Liu <charles.liu0309@gmail.com>
2022-08-22 04:25:15 -0400

    completed unsupervised learning week 1 anomaly detection

---
+++ unsupervised_learning/anomaly-detection.py
@@ -0,0 +1,89 @@
+# UNQ_C1
+# GRADED FUNCTION: estimate_gaussian
+def estimate_gaussian(X): 
+    """
+    Calculates mean and variance of all features 
+    in the dataset
+    
+    Args:
+        X (ndarray): (m, n) Data matrix
+    
+    Returns:
+        mu (ndarray): (n,) Mean of all features
+        var (ndarray): (n,) Variance of all features
+    """
+
+    m, n = X.shape
+    
+    ### START CODE HERE ###
+    
+    mu =np.sum(X, axis = 0) / m
+    var = np.sum((X - mu) ** 2, axis = 0) / m
+    
+    ### END CODE HERE ### 
+        
+    return mu, var
+
+# UNQ_C2
+# GRADED FUNCTION: select_threshold
+def select_threshold(y_val, p_val): 
+    """
+    Finds the best threshold to use for selecting outliers 
+    based on the results from a validation set (p_val) 
+    and the ground truth (y_val)
+    
+    Args:
+        y_val (ndarray): Ground truth on validation set
+        p_val (ndarray): Results on validation set
+        
+    Returns:
+        epsilon (float): Threshold chosen 
+        F1 (float):      F1 score by choosing epsilon as threshold
+    """ 
+
+    best_epsilon = 0
+    best_F1 = 0
+    F1 = 0
+    
+    step_size = (max(p_val) - min(p_val)) / 1000
+    
+    for epsilon in np.arange(min(p_val), max(p_val), step_size):
+    
+        ### START CODE HERE ###
+        predictions = (p_val < epsilon)
+
+        tp = np.sum(predictions & y_val)
+        fp = np.sum(predictions & np.logical_not(y_val))
+        fn = np.sum(np.logical_not(predictions) & y_val)
+        
+        prec = tp / (tp + fp)
+        rec = tp / (tp + fn)
+        
+        F1 = 2 * prec * rec / (prec + rec)
+        ### END CODE HERE ### 
+        
+        if F1 > best_F1:
+            best_F1 = F1
+            best_epsilon = epsilon
+        
+    return best_epsilon, best_F1
+
+
+
+### Anomaly Detection
+
+# Estimate the Gaussian parameters
+mu_high, var_high = estimate_gaussian(X_train_high)
+
+# Evaluate the probabilites for the training set
+p_high = multivariate_gaussian(X_train_high, mu_high, var_high)
+
+# Evaluate the probabilites for the cross validation set
+p_val_high = multivariate_gaussian(X_val_high, mu_high, var_high)
+
+# Find the best threshold
+epsilon_high, F1_high = select_threshold(y_val_high, p_val_high)
+
+print('Best epsilon found using cross-validation: %e'% epsilon_high)
+print('Best F1 on Cross Validation Set:  %f'% F1_high)
+print('# Anomalies found: %d'% sum(p_high < epsilon_high))
